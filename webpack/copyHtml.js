8ac687b9448860a01420cd5f5052aaa72f307683
xianbx <378434086@qq.com>
2018-08-02 17:35:10 +0800

    add es6 standard in copyHtml.js

--- webpack/copyHtml.js
+++ webpack/copyHtml.js
@@ -1,22 +1,43 @@
-const fs = require('fs')
+import fs from 'fs'
+import path from 'path'
 
-fs.readFile('./src/html/index.html', 'utf-8', function(err, data) {
-  if (err) {
-    throw err
-  }
+const htmlDir = path.join(__dirname, '../src/html')
+const exts = [
+  '.html'
+]
 
-  fs.mkdir('./dist/', function(err) {
-    if (err) {
-      return console.error(err)
-    }
-    console.log('目录创建成功。')
+// read files list
+let readFiles = () => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(htmlDir, (err, files) => {
+      resolve(files.filter((v) => exts.includes(path.parse(v).ext)))
+    })
   })
+}
 
-  fs.writeFile('./dist/index.html', data, function(err) {
-    if (err) {
-      console.error(err)
-    } else {
-      console.log('写入成功')
-    }
-  })
-});
+//  run
+;(async () => {
+  let files = await readFiles()
+  console.log(files)
+})()
+
+// fs.readFile('./src/html/index.html', 'utf-8', function(err, data) {
+//   if (err) {
+//     throw err
+//   }
+// })
+//
+// fs.mkdir('./dist/', function(err) {
+//   if (err) {
+//     return console.error(err)
+//   }
+//   console.log('目录创建成功。')
+// })
+//
+// fs.writeFile('./dist/index.html', data, function(err) {
+//   if (err) {
+//     console.error(err)
+//   } else {
+//     console.log('写入成功')
+//   }
+// })
