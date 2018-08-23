b1b1f5c968999a63f5cd1ea69f7822eaac52d077
xianbx <378434086@qq.com>
2018-08-23 16:55:12 +0800

    project begin

--- src/sphere/ts/main.ts
+++ src/sphere/ts/main.ts
@@ -1,48 +1,22 @@
 import * as style from '../scss/reset.scss'
 import * as back from '../assets/back.png'
-// import * as THREE from 'three';
-//
-// class Student {
-//   fullName: string;
-//   constructor(public firstName, public middleInitial, public lastName) {
-//     this.fullName = firstName + " " + middleInitial + " " + lastName;
-//     console.log(THREE)
-//
-//   }
-// }
-//
-// interface Person {
-//   firstName: string;
-//   lastName: string;
-// }
-//
-// function greeter(person: Person) {
-//   return "Hello, " + person.firstName + " " + person.lastName;
-// }
-//
-// let user = new Student("Jane", "M.", "User");
-//
-// document.body.innerHTML = greeter(user);
+import * as THREE from 'three'
+import Game from './game'
 
-// const game = new Game()
-// game.init()
-
-<<<<<<< HEAD
 const canvas = document.createElement('canvas')
-canvas.className = style.container;
-canvas.id = 'Canvas';
-=======
-// console.log('../' + back)
->>>>>>> 562a6343b9e28a68b2cd3d49df917e173c8646e2
+canvas.id = 'Canvas'
 
-const img = new Image();
-img.onload = function() {
-  const canvas = document.createElement('canvas')
-  canvas.className = style.container;
-  const ctx = canvas.getContext("2d");
-  ctx.drawImage(img, 100, 100);
+let game = new Game({ el: '#Canvas' })
+game.greet()
 
-  document.body.appendChild(canvas)
-  // document.body.appendChild(img)
-}
-img.src = '../' + back;
+document.body.appendChild(canvas)
+
+// const img = new Image()
+// img.onload = function() {
+//   canvas.className = style.container
+//   const ctx = canvas.getContext("2d")
+//   ctx.drawImage(img, 100, 100)
+//
+//   // document.body.appendChild(img)
+// }
+// img.src = '../' + back
