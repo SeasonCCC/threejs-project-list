15d08834688ec7e7aa36612d2c78eb41afe6753b
xianbx <378434086@qq.com>
2018-10-10 09:16:14 +0800

    new tsconfig.json

--- src/panorima/ts/main.ts
+++ src/panorima/ts/main.ts
@@ -1,21 +1,21 @@
 import '../scss/_mixin.scss'
 
 class Student {
-    fullName: string;
-    constructor(public firstName, public middleInitial, public lastName) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
+  public fullName: string
+  constructor (public firstName, public middleInitial, public lastName) {
+    this.fullName = firstName + ' ' + middleInitial + ' ' + lastName
+  }
 }
 
-interface Person {
-    firstName: string;
-    lastName: string;
+interface IPerson {
+  firstName: string
+  lastName: string
 }
 
-function greeter(person : Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function greeter (person: IPerson) {
+  return 'Hello, ' + person.firstName + ' ' + person.lastName
 }
 
-let user = new Student("Jane", "M.", "User");
+const user = new Student('Jane', 'M.', 'User')
 
-document.body.innerHTML = greeter(user);
+document.body.innerHTML = greeter(user)
